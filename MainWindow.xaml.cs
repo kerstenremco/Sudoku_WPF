f5f8741554fc1d69972d61b02a8c91003a69c93a
Remco Kersten <kersten.remco@outlook.com>
2020-04-13 22:06:42 +0200

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Sudoku_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Sudoku sudoku;
+        Button selectedButton;
+        int selectedNum;
+        public MainWindow()
+        {
+            InitializeComponent();
+            sudoku = new Sudoku();
+            InitGrid();
+            
+        }
+        public void InitGrid()
+        {
+
+            // Define the Columns
+            for(int i = 0; i < 9; i++)
+            {
+                ColumnDefinition col = new ColumnDefinition();
+                grid.ColumnDefinitions.Add(col);
+            }
+
+            // Define the Columns
+            for (int i = 0; i < 9; i++)
+            {
+                RowDefinition row = new RowDefinition();
+                grid.RowDefinitions.Add(row);
+            }
+
+
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    Label label = new Label();
+                    label.FontSize = 24;
+                    label.BorderBrush = Brushes.Black;
+                    label.MouseUp += new MouseButtonEventHandler(pnlMainGrid_MouseUp);
+                    Grid.SetRow(label, i);
+                    Grid.SetColumn(label, j);
+                    if (sudoku.board[i, j] > 0)
+                    {
+                        label.Content = sudoku.board[i, j];
+                    }
+                    if((j + 1) % 3 == 0 && (i + 1) % 3 == 0)
+                    {
+                        label.BorderThickness = new Thickness(0, 0, 3, 3);
+                    }
+                    else if ((j+1) % 3 == 0)
+                    {
+                        label.BorderThickness = new Thickness(0, 0, 3, 0);
+                    }
+                    else if ((i + 1) % 3 == 0)
+                    {
+                        label.BorderThickness = new Thickness(0, 0, 0, 3);
+                    }
+                    grid.Children.Add(label);
+                }
+            }
+        }
+
+        private void losOpClick(object sender, RoutedEventArgs e)
+        {
+            if(!sudoku.checkValidBoard())
+            {
+                // bord klopt niet
+                MessageBox.Show("Dit is een ongeldige soduku!");
+                return;
+            }
+            if (sudoku.SolveSoduku())
+            {
+                grid.RowDefinitions.Clear();
+                grid.ColumnDefinitions.Clear();
+                grid.Children.Clear();
+                InitGrid();
+            }
+            else
+            {
+                MessageBox.Show("Deze kan niet worden opgelost!");
+            }
+        }
+
+        private void voorbeeldClick(object sender, RoutedEventArgs e)
+        {
+            sudoku.loadExample();
+            grid.RowDefinitions.Clear();
+            grid.ColumnDefinitions.Clear();
+            grid.Children.Clear();
+            InitGrid();
+        }
+
+        private void resetClick(object sender, RoutedEventArgs e)
+        {
+            sudoku.reset();
+            grid.RowDefinitions.Clear();
+            grid.ColumnDefinitions.Clear();
+            grid.Children.Clear();
+            InitGrid();
+        }
+
+        private void pnlMainGrid_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+
+            int row = Grid.GetRow((UIElement)e.Source);
+            int col = Grid.GetColumn((UIElement)e.Source);
+            sudoku.board[row, col] = selectedNum;
+            Label label = sender as Label;
+            label.Content = selectedNum > 0 ? selectedNum.ToString() : null;
+        }
+
+        private void numClick(object sender, RoutedEventArgs e)
+        {
+            if (selectedButton != null)
+            {
+                selectedButton.Background = Brushes.Transparent;
+            }
+            selectedButton = sender as Button;
+            selectedButton.Background = Brushes.LightSkyBlue;
+            selectedNum = Convert.ToInt32(selectedButton.Content);
+        }
+    }
+}
---
+++ Sudoku.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sudoku_WPF
+{
+    class Sudoku
+    {
+        public int[,] board = new int[9, 9];
+        public Sudoku()
+        {
+            reset();
+        }
+
+        public bool SolveSoduku()
+        {
+            int[] emptyPosition = CheckForEmptyField();
+            if (emptyPosition[0] == -1 && emptyPosition[1] == -1)
+            {
+                // alle velden gevuld
+                return true;
+            }
+
+            // leeg veld gevonden, kijk welk cijfer veilig is en plaats deze
+            for (int num = 1; num <= 9; num++)
+            {
+                if (IsSafe(emptyPosition[0], emptyPosition[1], num))
+                {
+                    // cijfer kan veilig worden geplaatst
+                    board[emptyPosition[0], emptyPosition[1]] = num;
+                    if (SolveSoduku())
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        board[emptyPosition[0], emptyPosition[1]] = 0;
+                    }
+                }
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Zijn er nog lege velden in het bord?
+        /// </summary>
+        /// <param name="board">Sodukubord</param>
+        /// <returns>Return [-1, -1] als bord gevuld is, indien er een lege plek is gevonden, return [row, col] van de positie</returns>
+        public int[] CheckForEmptyField()
+        {
+            int[] position = { -1, -1 };
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    if (board[i, j] == 0)
+                    {
+                        position[0] = i;
+                        position[1] = j;
+                        return position;
+                    }
+                }
+            }
+            return position;
+        }
+
+        /// <summary>
+        /// Controleer of een cijfer veilig kan worden geplaatst zonder conflicten
+        /// </summary>
+        /// <param name="board">Bord</param>
+        /// <param name="row">Index rij</param>
+        /// <param name="col">Index colom</param>
+        /// <param name="num">Het te plaatsen nummer</param>
+        /// <returns>Return true indien veilig, false indien onveilig</returns>
+        public bool IsSafe(int row, int col, int num)
+        {
+            // kijk of cijfer in rij voorkomt, zo ja, return false
+            for (int c = 0; c < 9; c++)
+            {
+                if (board[row, c] == num && c != col)
+                {
+                    return false;
+                }
+            }
+
+            // kijk of cijfer voorkomt in col, zo ja, return false
+            for (int r = 0; r < 9; r++)
+            {
+                if (board[r, col] == num && r != row) return false;
+            }
+
+            // kijk of cijfer in box voorkomt
+            int boxRow = (int)Math.Floor(row / 3.0);
+            int boxCol = (int)Math.Floor(col / 3.0);
+            int startAtRow = boxRow * 3;
+            int startAtCol = boxCol * 3;
+            for (int r = startAtRow; r < startAtRow + 3; r++)
+            {
+                for (int c = startAtCol; c < startAtCol + 3; c++)
+                {
+                    if (board[r, c] == num && r != row && c != col) return false;
+                }
+            }
+            return true;
+        }
+
+        public bool checkValidBoard()
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    int num = board[i, j];
+                    if(num > 0)
+                    {
+                        if (!IsSafe(i, j, num))
+                        {
+                            Console.WriteLine("Ongeldig bord");
+                            return false;
+                        }
+                    }
+                }
+            }
+            return true;
+        }
+
+        public void printBoard()
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    Console.Write(board[i, j]);
+                }
+                Console.WriteLine(board[i, 8]);
+            }
+        }
+
+        public void loadExample()
+        {
+            board = new int[,]
+            {
+                {0, 0, 0, 0, 6, 0, 8, 2, 5},
+                {0, 0, 2, 8, 3, 5, 0, 0, 7},
+                {5, 0, 9, 0, 0, 0, 0, 0, 0},
+                {0, 2, 8, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 7, 8, 9, 0, 0, 2},
+                {0, 3, 0, 0, 0, 1, 4, 0, 6},
+                {3, 0, 0, 0, 0, 0, 0, 4, 0},
+                {0, 0, 4, 1, 0, 3, 0, 9, 0},
+                {0, 6, 0, 0, 9, 2, 7, 0, 0}
+            };
+        }
+
+        public void reset()
+        {
+            board = new int[9, 9];
+    }
+    }
+}
